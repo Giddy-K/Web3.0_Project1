d63060bd6521d67c08bc42070591e90758302414
Giddy-K <gideonkipamet@gmail.com>
2023-11-07 20:01:50 +0300

    finished navbar

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,43 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { Loader } from "./";
+
+const Welcome = () => {
+  const connectWallet = () => {};
+
+  return (
+    <div className="flex w-full justify-center items-center">
+      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col md:mr-10">
+          <h1 className="text-3xl sm:text-5xl text-white p-1">
+            <span role="img" aria-label="wave emoji">
+              👋 Hello!!!
+            </span>
+          </h1>
+          <h1 className="text-3xl sm:text-5xl text-white text-gradient p-1">
+            Send Ehererum <br /> across the world
+          </h1>
+          <p className="text-left mt-5 text-white font-light md:9/12 w-11/12 text-base">
+            Explore the crypto world and sell cryptocurrencies easilly on Krypto
+          </p>
+          <button
+            type="button"
+            onClick={connectWallet}
+            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+          >
+            <p className="text-white text-base font-semibold">Connect Wallet</p>
+          </button>
+          <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
+            <div > 
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
