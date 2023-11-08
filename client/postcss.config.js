a92c3d396bf481b098832b009aa3009c74ad6421
Giddy-K <gideonkipamet@gmail.com>
2023-11-08 17:11:39 +0300

    Finished welcome layout

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,2 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import tailwindcss from "tailwindcss";
+export const plugins = [tailwindcss("./tailwind.config.js"), ("autoprefixer")];
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,33 @@
-/** @type {import('tailwindcss').Config} */
-export const content = [
-  "./src/**/*.{js,jsx,ts,tsx}",
-];
+export const purge = ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"];
+export const mode = "jit";
+export const darkMode = false;
 export const theme = {
+  fontFamily: {
+    display: ["Open Sans", "sans-serif"],
+    body: ["Open Sans", "sans-serif"],
+  },
+  extend: {
+    screens: {
+      mf: "990px",
+    },
+    keyframes: {
+      "slide-in": {
+        "0%": {
+          "-webkit-transform": "translateX(120%)",
+          transform: "translateX(120%)",
+        },
+        "100%": {
+          "-webkit-transform": "translateX(0%)",
+          transform: "translateX(0%)",
+        },
+      },
+    },
+    animation: {
+      "slide-in": "slide-in 0.5s ease-out",
+    },
+  },
+};
+export const variants = {
   extend: {},
 };
-export const plugins = [];
+export const plugins = [("@tailwindcss/forms")];
